ec05572f74c3e7e8f2a5fb9fafc32412cbcc4260
Mr. Kiedes <akiedes@vtsd.com>
2022-09-30 09:08:05 -0400

    Lab Start Suggestion

--- Lab04avst.java
+++ Lab04avst.java
@@ -12,7 +12,11 @@ public class Lab04avst
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
-
+        double numMonths = numYears * 12;
+        // Convert to decimal, then months
+        double monthlyRate = (annualRate / 100) / 12;
+        double numerator = monthlyRate * Math.pow(1 + monthlyRate, numMonths);
+        System.out.println(numerator);
 
 
 
